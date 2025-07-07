0bbacb9cc4eb4f3383b3c9ce22728feaa92c2ae9
acodexpetkov <56339072+acodexpetkov@users.noreply.github.com>
2025-07-07 07:55:32 +0200

    Update Program.cs to final presentation version

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Change the text here to update your visible version
-app.MapGet("/", () => "Hello World version Final with ECS ver 7 !!!!");
+app.MapGet("/", () => "Hello World version Final for Presentation ver.1");
 
 app.Run();
