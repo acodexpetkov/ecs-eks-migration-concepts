725cd426dd51bc504038e7ea1127261867db5e2e
acodexpetkov <56339072+acodexpetkov@users.noreply.github.com>
2025-07-07 08:38:04 +0200

    Update Program.cs
    DEPLOY_ECS set to false

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Change the text here to update your visible version
-app.MapGet("/", () => "Hello World version Final for Presentation ver.1");
+app.MapGet("/", () => "Hello World ver.1");
 
 app.Run();
