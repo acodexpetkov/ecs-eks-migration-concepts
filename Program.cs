91404fbb725e3e1f7e82fbd37fa2faa0d947d4b4
acodexpetkov <56339072+acodexpetkov@users.noreply.github.com>
2025-07-08 20:20:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Change the text here to update your visible version
-app.MapGet("/", () => "Hello World Final ver.11");
+app.MapGet("/", () => "Hello World Final ver.12");
 
 app.Run();
